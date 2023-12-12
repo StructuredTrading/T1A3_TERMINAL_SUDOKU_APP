6e9619ed2b8cfe898591bb566ea714e76b5f1251
StructuredTrading <samowen1992@hotmail.com>
2023-12-12 22:51:46 +1000

    Added solve_sudoku function to functions.py

--- functions.py
+++ functions.py
@@ -8,6 +8,34 @@ def submit_details():
 
 
 
+def solve_sudoku(grid):
+    """
+    Solve a Sudoku puzzle using a backtracking algorithm.
+
+    Parameters:
+    - grid (list): A 9x9 Sudoku grid represented as a list of lists.
+      Empty cells are denoted by 0.
+
+    Returns:
+    - bool: True if a solution is found, False otherwise.
+
+    The function employs a recursive backtracking algorithm to solve a given Sudoku puzzle.
+    It iterates through each cell in the grid, attempting to fill in missing numbers.
+    For each empty cell, it tries different numbers and recursively explores potential solutions.
+    If a valid number is found for a cell, the function proceeds to solve the remaining puzzle.
+    If a solution is not found, it backtracks and tries a different number, repeating the process.
+    """
+    for _row in range(9):
+        for _col in range(9):
+            if grid[_row][_col] == 0:
+                for num in range(1, 10):
+                    if(is_valid_move(grid, _row, _col, num)):
+                        grid[_row][_col] = num
+                        if solve_sudoku(grid):
+                            return True
+                        grid[_row][_col] = 0
+                return False
+    return True
 
 
 # Generates a sudoku grid and returns it
